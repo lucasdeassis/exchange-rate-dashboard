16ad21ff1f3292fcc78089dfbaf3298479352f7d
moiseshilario <moiseshilariu@gmail.com>
2017-11-27 14:35:55 -0200

    Clean componentDidMount

--- exchange-rate-dashboard-app/src/App.js
+++ exchange-rate-dashboard-app/src/App.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import logo from './business.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './business.svg'
+import './App.css'
 
-import CardCurrency from './components/card_currency.js';
-import dolar from './dolar.js';
+import CardCurrency from './components/card_currency.js'
+import dolar from './dolar.js'
 import euro from './euro.js'
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line } from 'recharts'
+import tinydate from 'tinydate'
+
+
 
 class App extends Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       dolar : 0,
       euro : 0,
       euromeses : [],
       bitcoin : 0
-    };
+    }
 
   }
 
@@ -31,71 +34,37 @@ class App extends Component {
         euro: data.rates.BRL
       })
     })
-    euro.getMonth('01').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-          this.setState({
-              euromeses
-          })
-    })
-    euro.getMonth('02').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('03').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('04').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('05').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('06').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('07').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('08').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('09').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
-    euro.getMonth('10').then( data => {
-      let euromeses = [ data.rates.BRL, ...this.state.euromeses]
-      this.setState({
-          euromeses
-      })
-    })
 
+    const months = this.initializeMonths()
+
+    Promise.all(months.map(month => euro.getMonth(month)))
+      .then((euroResponse) => {
+        const euromeses = []
+        for (let i = 0; i < euroResponse.length; i++) {
+          const { rates } = euroResponse[i];
+          euromeses.push(rates.BRL)
+        }
+        this.setState({ euromeses })
+      })
+
+    }
+
+  initializeMonths() {
+    const stamp = tinydate('{MM}')
+    const months = []
+    const currentMonth = new Date().getMonth()
+
+    for (let i = 0; i < currentMonth; i++) {
+      const date = new Date()
+      date.setMonth(i)
+      months.push(stamp(date))
+    }
+    return months
   }
 
   render() {
-    console.log(this.state.euromeses.map( euromes => ({euromes}) ));
+    console.log('euromeses', this.state.euromeses)
+    console.log(this.state.euromeses.map( euromes => ({euromes}) ))
     return (
       <div className="App">
         <header className="App-header">
@@ -138,11 +107,11 @@ class App extends Component {
         </div>
         <LineChart width={400} height={400} data={this.state.euromeses.map( euromes => ({euromes}) )}>
           <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-          </LineChart>
+        </LineChart>
       </div>
 
-    );
+    )
   }
 }
 
-export default App;
+export default App
